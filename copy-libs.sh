40b7a2e4c43b549f1a8c78455086916c55521431
4linuxhd <robertof.souza@bol.com.br>
2017-08-08 10:23:48 -0300

    Update copy-libs.sh

--- copy-libs.sh
+++ copy-libs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #### Sqoop com suporte a MySQL
 hdfs dfs -put -f /usr/local/sqoop/lib/mysql-connector-java-5.1.38-bin.jar /user/hduser/share/lib/lib*/sqoop
 
@@ -9,7 +10,6 @@ hdfs dfs -put -f /usr/local/hbase/lib/high* /user/hduser/share/lib/lib*/sqoop
 hdfs dfs -put -f /usr/local/hbase/lib/htrace* /user/hduser/share/lib/lib*/sqoop
 
 #### Hive com suporte a HBase
-hdfs dfs -put -f /usr/local/hbase/lib/*hbase* /user/hduser/share/lib/lib*/hive
 hdfs dfs -put -f /usr/local/hive/lib/*hbase* /user/hduser/share/lib/lib*/hive
 hdfs dfs -put -f /usr/local/hive/lib/metrics* /user/hduser/share/lib/lib*/hive
 hdfs dfs -put -f /usr/local/hive/lib/hive-hbase* /user/hduser/share/lib/lib*/hive
@@ -30,11 +30,8 @@ hdfs dfs -put -f /usr/local/tez/*.jar /user/hduser/share/lib/lib*/pig
 hdfs dfs -put -f /usr/local/tez/lib/common* /user/hduser/share/lib/lib*/pig
 
 #### Pig com suporte a Hive HCatalog
-hdfs dfs -put -f /usr/local/hive/lib/hive-comm* /user/hduser/share/lib/lib*/pig
-hdfs dfs -put -f /usr/local/hive/lib/hive-exec* /user/hduser/share/lib/lib*/pig
-hdfs dfs -put -f /usr/local/hive/lib/hive-serd* /user/hduser/share/lib/lib*/pig
-hdfs dfs -put -f /usr/local/hive/lib/hive-shims-com* /user/hduser/share/lib/lib*/pig
-hdfs dfs -put -f /usr/local/hive/lib/hive-meta* /user/hduser/share/lib/lib*/pig
+hdfs dfs -put -f /usr/local/pig/lib/hive-* /user/hduser/share/lib/lib*/pig
+hdfs dfs -put -f /usr/local/pig/lib/h2/hive-* /user/hduser/share/lib/lib*/pig
 hdfs dfs -put -f /usr/local/hive/lib/lib* /user/hduser/share/lib/lib*/pig
 hdfs dfs -put -f /usr/local/hive/hcatalog/share/hcatalog/* /user/hduser/share/lib/lib*/pig
 hdfs dfs -put -f /usr/local/hive/hcatalog/share/webhcat/java-client/* /user/hduser/share/lib/lib*/pig
